61f1c94fd992a0d36125cff6891600312ccac47e
ramankarki <ramankarki40@gmail.com>
2024-02-02 13:24:14 +0545

    initial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native'
+import tw from 'twrnc'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View>
+			<View style={tw`h-[30%]`}>
+				<View
+					style={tw`flex items-center justify-center bg-gray-100 mt-8 max-w-[200px] h-[200px] mx-auto w-full rounded-full`}
+				>
+					<Text>Home</Text>
+				</View>
+			</View>
+			<View style={tw`h-[70%] border-4 flex justify-center items-center gap-4`}>
+				<View style={tw`flex flex-row gap-4`}>
+					<View style={tw`w-10 h-10 bg-black`}></View>
+					<View style={tw`w-10 h-10 bg-black`}></View>
+				</View>
+				<View style={tw`flex flex-row gap-4`}>
+					<View style={tw`w-10 h-10 bg-black`}></View>
+					<View style={tw`w-10 h-10 bg-black`}></View>
+				</View>
+			</View>
+		</View>
+	)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
